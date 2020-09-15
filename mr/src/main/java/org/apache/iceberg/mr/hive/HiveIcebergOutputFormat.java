31c547b2391251b32025c75a96e4eb884e5a106f
Peter Vary <pvary@cloudera.com>
2020-09-15 09:56:43 +0200

    Fix checkstyle
    (cherry picked from commit 7a8be308335b8f88f505c5a914592ae4011f92de)

--- mr/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergOutputFormat.java
+++ mr/src/main/java/org/apache/iceberg/mr/hive/HiveIcebergOutputFormat.java
@@ -23,8 +23,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.nio.ByteBuffer;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Properties;
